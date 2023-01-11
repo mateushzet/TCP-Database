971380bdf51b924d38a84b1e4afd2a0b7bdbcbae
mateushzet <72745420+mateushzet@users.noreply.github.com>
2023-01-11 18:57:09 +0100

    Add files via upload

--- src/ServerTCP.java
+++ src/ServerTCP.java
@@ -6,15 +6,14 @@ import java.util.ArrayList;
 public class ServerTCP {
     public static void main(String args[]) throws SQLException {
 
-            String url = "jdbc:mariadb://localhost:3306/kolokwium";
-            String user = "root";
-            String pwd = "password";
-            Connection con = DriverManager.getConnection(url,user,pwd);
-
+        String url = "jdbc:mariadb://localhost:3306/kolokwium";
+        String user = "root";
+        String pwd = "password";
+        Connection con = DriverManager.getConnection(url,user,pwd);
         String[] tempArgs = new String[1];
         tempArgs[0] = "4999";
         final int MAX_CLIENT_CAPACITY = 250;
-
+        boolean quit = false;
         if (tempArgs.length == 0)
             System.out.println("Wprowadz numer portu, na ktorym" + "serwer bpdzie oczekiwai na klientOw");
         else {
@@ -26,36 +25,37 @@ public class ServerTCP {
                 return;
             }
 
-        try(ServerSocket serverSocket = new ServerSocket(port)) {
-            int i = 0;
-            while (true) {
-                if(ServerTCPThread.connectedClients < MAX_CLIENT_CAPACITY) {
-                    ServerTCPThread thread = new ServerTCPThread(serverSocket.accept(),con);
-                    i++;
-                    ServerTCPThread.connectedClients++;
-                    System.out.println("polaczono klienta nr: " + i);
-                    thread.start();
-                }else System.out.println("Wszystkie miejsca zajete");
-          }
-
+            try(ServerSocket serverSocket = new ServerSocket(port)) {
+                int i = 0;
+                while (!quit) {
+                    if(ServerTCPThread.connectedClients < MAX_CLIENT_CAPACITY) {
+                        ServerTCPThread thread = new ServerTCPThread(serverSocket.accept(),con);
+                        i++;
+                        ServerTCPThread.connectedClients++;
+                        System.out.println("polaczono klienta nr: " + i);
+                        thread.start();
+                    }else System.out.println("Wszystkie miejsca zajete");
+                }
+                serverSocket.close();
 
             }catch (Exception e)
             { System.err.println(e); }
         }
     }
 
+
     static public void initTables(Connection con) throws SQLException {
-        String tables [] = new String[4];
-        tables[0] = "Create table odpowiedzi_uczniów (idOdpowiedzi INT PRIMARY KEY AUTO_INCREMENT, imie varchar(30) NOT NULL, odpowiedz varchar(30), numerPytania INT(2) NOT NULL)";
-        tables[1] = "Create table pytania (numerPytania INT(2) PRIMARY KEY, pytanie varchar(100) NOT NULL, a varchar(100) NOT NULL, b varchar(100) NOT NULL, c varchar(100) NOT NULL, d varchar(100) NOT NULL)";
-        tables[2] = "Create table poprawne_odpowiedzi (numerPytania INT(2) PRIMARY KEY, odpowiedz varchar(30) NOT NULL)";
-        tables[3] = "Create table wyniki (idWyniku INT PRIMARY KEY AUTO_INCREMENT, imie varchar(30) NOT NULL, punkty INT(2) NOT NULL)";
-
+//        String tables [] = new String[4];
+//        tables[0] = "Create table odpowiedzi_uczniów (idOdpowiedzi INT PRIMARY KEY AUTO_INCREMENT, imie varchar(30) NOT NULL, odpowiedz varchar(30), numerPytania INT(2) NOT NULL)";
+//        tables[1] = "Create table pytania (numerPytania INT(2) PRIMARY KEY, pytanie varchar(100) NOT NULL, a varchar(100) NOT NULL, b varchar(100) NOT NULL, c varchar(100) NOT NULL, d varchar(100) NOT NULL)";
+//        tables[2] = "Create table poprawne_odpowiedzi (numerPytania INT(2) PRIMARY KEY, odpowiedz varchar(30) NOT NULL)";
+//        tables[3] = "Create table wyniki (idWyniku INT PRIMARY KEY AUTO_INCREMENT, imie varchar(30) NOT NULL, punkty INT(2) NOT NULL)";
+//
         Statement stm = con.createStatement();
-
-        for (int i = 0; i < 4; i++) {
-            stm.executeUpdate(tables[i]);
-        }
+//
+//        for (int i = 0; i < 4; i++) {
+//            stm.executeUpdate(tables[i]);
+//        }
 
         String rows [] = new String[4];
 
